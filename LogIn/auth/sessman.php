2d800405d1ece129224145e4153cab9e3206e8e7
Mohamed Abu Samra <43873183+MohamedAbuSamra@users.noreply.github.com>
2021-06-23 17:10:22 +0300

    Update sessman.php

--- LogIn/auth/sessman.php
+++ LogIn/auth/sessman.php
@@ -1,6 +1,6 @@
 <?php
 
-//PHP file too keep activing the logged in for the user.
+//Keepiing the logged in on for the user.
 session_start();
 
 if(!isset($_SESSION['isLogged']) or '1' != $_SESSION['isLogged'])
